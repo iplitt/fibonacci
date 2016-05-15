e049a92ae62f55297dee59a00c37f34fc0588290
Irwin <iplitt@alum.mit.edu>
2016-05-14 17:01:48 -0700

    IP - The rest of the code.

---
+++ fibonacci/plitt.fibonacci.UnitTests/FibonacciGeneratorUnitTest.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+
+namespace plitt.fibonacci.UnitTests
+{
+    [TestFixture]
+    public class FibonacciGeneratorUnitTest
+    {
+        private FibonacciGenerator _testObject;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _testObject = new FibonacciGenerator();
+        }
+
+
+        [TestCase(0, 0)]
+        [TestCase(1, 1)]
+        [TestCase(2, 1)]
+        [TestCase(3, 2)]
+        [TestCase(4, 3)]
+        [TestCase(5, 5)]
+        [TestCase(6, 8)]
+        [TestCase(47, 2971215073)]
+        [TestCase(48, 4807526976)]
+        [TestCase(49, 7778742049)]
+        public void GetFibonaci_ReturnsValue(int n, long expected)
+        {
+            var actual = _testObject.GetFibonaci(n);
+            Assert.AreEqual(expected, actual);
+        }
+
+    }
+}
---
+++ fibonacci/plitt.fibonacci/FibonacciGenerator.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace plitt.fibonacci
+{
+    public class FibonacciGenerator
+    {
+        private Dictionary<int, long> _dict;
+
+        public FibonacciGenerator()
+        {
+            _dict = new Dictionary<int, long>();
+            _dict.Add(0, 0);
+            _dict.Add(1, 1);
+        }
+
+        public long GetFibonaci(int n)
+        {
+            if (_dict.ContainsKey(n))
+                return _dict[n];
+
+            long value = GetFibonaci(n - 1) + GetFibonaci(n - 2);
+            _dict.Add(n, value);
+            return value;
+        }
+
+    }
+}
